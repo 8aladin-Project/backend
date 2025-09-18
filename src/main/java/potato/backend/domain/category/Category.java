74bd86f5278791f3ecc4ab09e017d2b79eea7592
김윤기 <kalsagansteve@gmail.com>
2025-09-18 11:06:41 +0900

    feat: Add Category entity with creation method and JPA annotations

---
+++ src/main/java/potato/backend/domain/category/Category.java
@@ -0,0 +1,30 @@
+package potato.backend.domain.category;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_seq")
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String categoryName;
+
+    public static Category create(String categoryName){
+        Objects.requireNonNull(categoryName);
+
+        return Category.builder()
+            .categoryName(categoryName)
+            .build();
+    }
+}
