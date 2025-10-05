9d035c3e84e61aa9f23df08d9a5bb5710f6cecc0
daiseek <dnjsgml0578@gmail.com>
2025-10-05 14:27:50 +0900

    feat: 실시간 채팅 기능 구현

---
+++ src/main/java/potato/backend/domain/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,18 @@
+package potato.backend.domain.chat.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import potato.backend.domain.chat.domain.ChatRoom;
+import potato.backend.domain.user.domain.Member;
+
+@Repository
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("select cr from ChatRoom cr where cr.seller = :seller and cr.buyer = :buyer")
+    Optional<ChatRoom> findByParticipants(@Param("seller") Member seller, @Param("buyer") Member buyer);
+}
