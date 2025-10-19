f9a0a34dae18498931d3ee660e079a41a608f861
daiseek <dnjsgml0578@gmail.com>
2025-10-19 15:38:43 +0900

    fix: swagger에서 API 테스트를 위해 chat API의 경로는 인증 필터에서 제외

--- src/main/java/potato/backend/global/config/SecurityConfig.java
+++ src/main/java/potato/backend/global/config/SecurityConfig.java
@@ -53,7 +53,9 @@ public class SecurityConfig {
                                 "/v3/api-docs.yaml",
                                 "/ws-chat/**",  // WebSocket 엔드포인트 허용
                                 "/ws-chat",     // SockJS 엔드포인트 허용
-                                "/api/v1/wishlists/**"  // 위시리스트 API 임시 허용
+                                "/api/v1/wishlists/**",  // 위시리스트 API 임시 허용
+                                "/api/v1/chatrooms/**",  // 채팅 API 임시 허용
+                                "/api/v1/chat/**"  // 채팅 API 임시 허용
                         ).permitAll()
                         .anyRequest().authenticated() // 개발용
                 );
