ece3e88b30aa59b2b7a44df7d0e42b6703bae9a4
김윤기 <kalsagansteve@gmail.com>
2025-10-25 02:56:54 +0900

    feat: update application configuration for production environment

--- src/main/java/potato/backend/domain/category/domain/Category.java
+++ src/main/java/potato/backend/domain/category/domain/Category.java
@@ -10,6 +10,11 @@ import java.util.Objects;
 @Builder(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SequenceGenerator(
+    name = "category_seq",
+    sequenceName = "category_seq",
+    allocationSize = 50
+)
 public class Category {
 
     @Id
