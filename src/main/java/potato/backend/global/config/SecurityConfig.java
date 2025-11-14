5e382ddeff7dfd308346203aa7de8dbb533d360f
DHKim7762 <kim138762@gmail.com>
2025-11-14 22:58:47 +0900

    Fix cors 에러 해결/#59 (#60)
    * fix: SecurityConfig에 OPTIONS 요청 허용 추가 (CORS Pre-flight 요청 처리)
    
    * fix: JwtFilter에서 OPTIONS 요청 제외 처리 (CORS Pre-flight 요청 통과)

--- src/main/java/potato/backend/global/config/SecurityConfig.java
+++ src/main/java/potato/backend/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -43,6 +44,7 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
         http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
